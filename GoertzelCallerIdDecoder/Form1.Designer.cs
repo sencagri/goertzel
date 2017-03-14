886569998862ff305ca5a52e58b1ccecd2a54033
Ebubekir Çağrı ŞEN <ebubekircagrisen@gmail.com>
2017-03-14 17:40:50 +0300

    add : added another windows forms project to visualize decoded data coming from mic.

---
+++ GoertzelCallerIdDecoder/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace GoertzelCallerIdDecoder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startStopButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // startStopButton
+            // 
+            this.startStopButton.Location = new System.Drawing.Point(180, 12);
+            this.startStopButton.Name = "startStopButton";
+            this.startStopButton.Size = new System.Drawing.Size(92, 45);
+            this.startStopButton.TabIndex = 0;
+            this.startStopButton.Text = "Start/Stop Recording from Mic.";
+            this.startStopButton.UseVisualStyleBackColor = true;
+            this.startStopButton.Click += new System.EventHandler(this.startStopButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.startStopButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startStopButton;
+    }
+}
+
---
+++ GoertzelCallerIdDecoder/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DevExpress.XtraEditors;
+using NAudio.Wave;
+
+namespace GoertzelCallerIdDecoder
+{
+    public partial class Form1 : XtraForm
+    {
+        BufferedWaveProvider bwp;
+        private WaveInEvent wi;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void startStopButton_Click(object sender, EventArgs e)
+        {
+
+        }
+        public void NaudioSettings()
+        {
+            wi = new WaveInEvent();
+            wi.DataAvailable += new EventHandler<WaveInEventArgs>(wi_DataAvailable);
+            bwp = new BufferedWaveProvider(wi.WaveFormat);
+            bwp.DiscardOnBufferOverflow = true;
+            wi.StartRecording();
+        }
+        void wi_DataAvailable(object sender, WaveInEventArgs e)
+        {
+            bwp.AddSamples(e.Buffer, 0, e.BytesRecorded);
+        }
+    }
+}
